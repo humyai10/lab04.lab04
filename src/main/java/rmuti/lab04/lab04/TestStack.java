9a0f51168a7900d067d565bcfd443836831a4a69
senseye20794 <senseye20794@hotmail.com>
2018-01-17 21:55:38 +0700

    kuy

---
+++ src/main/java/rmuti/lab04/lab04/TestStack.java
@@ -0,0 +1,18 @@
+package rmuti.lab04.lab04;
+
+public class TestStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayListStack stack =new ArrayListStack();
+		stack.push("macos");
+		stack.push("linux");
+		stack.push("windows");
+		System.out.println(stack);
+		
+		String data = (String)stack.pop();
+		System.out.println(stack);
+
+	}
+
+}
